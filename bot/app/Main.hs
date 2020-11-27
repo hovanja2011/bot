2e036f8e9d713772d5745ae817301b8f73bed7ce
hovanja2011 <hovanja2011@mail.ru>
2020-11-27 10:38:06 +0300

    Add files via upload

---
+++ bot/app/Main.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+
+
+
+import           Network.HTTP.Simple            ( httpBS, getResponseBody )               
+import qualified Data.ByteString.Char8         as BS
+
+
+fetchJSON :: IO BS.ByteString
+fetchJSON = do
+  res <- httpBS "https://api.telegram.org/bot1421138697:AAHfmKgs38ODbldkqE3jlGEikQlaNuXsOXA/getUpdates"
+  return (getResponseBody res)
+
+
+main :: IO ()
+main = do
+  json <- fetchJSON
+  BS.putStrLn json
---
+++ bot/src/Lib.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lib
+ where
+ 
+--data User = User {
+--      ok :: Bool
+--    , result  :: [SMTH]
+--    } deriving (Generic, Show)
+
+
+
+--instance FromJSON User where
+--    parseJSON = withObject "User"$ \v -> User 
+--        <$> v .: "ok"
+--        <*> v .: "result"
+
+
+
+
+someFunc :: IO ()
+
+someFunc = putStrLn "someFunc"
