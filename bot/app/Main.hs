2bcf3713fc696373f751e136040969f891772ec2
hovanja2011 <hovanja2012@mail.ru>
2020-12-28 02:52:37 +0300

    I can't parse JSON from getUpdate

--- bot/app/Main.hs
+++ bot/app/Main.hs
@@ -10,15 +10,12 @@ import qualified Data.ByteString.Lazy.Char8 as L8
 import Network.HTTP.Simple
 import Data.Aeson
 import Data.Maybe
+import Data.List 
 --import Data.Text as T
 import GHC.Generics
 
---something from fizruk path
-data Action = NoAction
-data Model 
---end
 
--- everything for making request
+-- everything for making getMe request
 token :: B8.ByteString 
 token = "bot1421138697:AAHfmKgs38ODbldkqE3jlGEikQlaNuXsOXA"
 
@@ -45,18 +42,41 @@ request_getMe :: Request
 request_getMe = buildRequest tHost tPath_getMe
 --end
 
--- here I parse JSON User
+
+--building User
 data User = User {
     user_id         :: Int ,
     user_is_bot     :: Bool ,
     user_first_name :: String } deriving (Show)
-
 instance FromJSON User where
     parseJSON = withObject "user" $ \o -> do
-      result <- o.: "result"
-      user_id <- result .: "id"
-      user_is_bot <- result .: "is_bot"
-      user_first_name <- result .: "first_name"
+      user_ok <- o.: "ok"
+      result <- last <$> o.: "result"
+      message <- result .: "message"
+      from <- message .: "from"
+      user_id <- from .: "id"
+      user_is_bot <- from .: "is_bot"
+      user_first_name <- from .: "first_name"
+      return (User user_id user_is_bot user_first_name )
+
+
+newtype ResultList = ResultList {resultList :: [Result]}
+instance FromJSON ResultList where
+    parseJSON (Object o) = ResultList <$> o.: "result"
+    parseJSON _ = mzero
+
+
+data Result = Result {
+    result_id         :: Int ,
+    result_is_bot     :: Bool ,
+    result_first_name :: String } deriving (Show)
+instance FromJSON Result where
+    parseJSON = withObject "result" $ \o -> do
+      message <- o .: "message"
+      from <- message .: "from"
+      user_id <- from .: "id"
+      user_is_bot <- from .: "is_bot"
+      user_first_name <- from .: "first_name"
       return (User user_id user_is_bot user_first_name )
 
 getUser :: Request -> IO (User)
@@ -64,6 +84,7 @@ getUser request = do
   t <- httpJSON request
   return (getResponseBody t)
 -- end
+
 
 --here I send message to User
 message_help :: B8.ByteString
@@ -85,11 +106,25 @@ get_message_response id = do
 --Problem. I send message from bot to bot!
 --end
 
+
+-- make getUpdate request
+method_getUpdate :: B8.ByteString
+method_getUpdate = "/getUpdate"
+
+tPath_getUpdate :: B8.ByteString
+tPath_getUpdate = makePath token method_getUpdate
+
+request_getUpdate :: Request
+request_getUpdate = buildRequest tHost tPath_getUpdate
+-- want to update/universied making request methods 
+--end
+
+
 main :: IO ()
 main = do
-    user <- getUser request_getMe
-    id <- return (user_id user)
-    body <- get_message_response id
-    print (body )
+    user <- getUser request_getMe :: IO (User)
+    iid <- return (is_bot user)
+   -- body <- get_message_response id
+    print (iid )
 --https://api.telegram.org/bot1421138697:AAHfmKgs38ODbldkqE3jlGEikQlaNuXsOXA/getMe
 
