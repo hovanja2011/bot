b414ac8545a514763b43c4619113e9cad64df24f
hovanja2011 <hovanja2012@mail.ru>
2020-12-28 23:24:25 +0300

    still can't get user_id

--- bot/app/Main.hs
+++ bot/app/Main.hs
@@ -15,69 +15,84 @@ import Data.List
 import GHC.Generics
 
 
--- everything for making getMe request
+-- everything for making request (getMe and getUpdate)
+data Method = GetMe | GetUpdate
+newtype Path = Path B8.ByteString
+
 token :: B8.ByteString 
 token = "bot1421138697:AAHfmKgs38ODbldkqE3jlGEikQlaNuXsOXA"
 
-method_getMe :: B8.ByteString
-method_getMe = "/getMe"
+path:: Method -> Path
+path m = case m of
+    GetMe -> Path (B8.append token "/getMe")
+    GetUpdate -> Path (B8.append token "/getUpdate")
 
-makePath :: B8.ByteString -> B8.ByteString -> B8.ByteString
-makePath = B8.append
+host :: B8.ByteString
+host = "api.telegram.org"
 
-tHost :: B8.ByteString
-tHost = "api.telegram.org"
-
-tPath_getMe :: B8.ByteString
-tPath_getMe = makePath token method_getMe
+request ::  Method -> Request
+request m = buildRequest host p where Path p = path m
 
 buildRequest :: B8.ByteString -> B8.ByteString -> Request
-buildRequest host path = setRequestHost host
+buildRequest host' path = setRequestHost host'
   $ setRequestPath path
   $ setRequestSecure True
   $ setRequestPort 443
   $ defaultRequest
-
-request_getMe :: Request
-request_getMe = buildRequest tHost tPath_getMe
 --end
-
-
---building User
 data User = User {
-    user_id         :: Int ,
-    user_is_bot     :: Bool ,
-    user_first_name :: String } deriving (Show)
+  id_update :: Int
+} deriving Show
+
 instance FromJSON User where
-    parseJSON = withObject "user" $ \o -> do
-      user_ok <- o.: "ok"
-      result <- last <$> o.: "result"
-      message <- result .: "message"
-      from <- message .: "from"
-      user_id <- from .: "id"
-      user_is_bot <- from .: "is_bot"
-      user_first_name <- from .: "first_name"
-      return (User user_id user_is_bot user_first_name )
+  parseJSON = withObject "user" $ \o -> do
+    res <- o.: "result"
+    id_update <- res.: "update_id"
+    return (User id_update)
+--building User
 
 
-newtype ResultList = ResultList {resultList :: [Result]}
-instance FromJSON ResultList where
-    parseJSON (Object o) = ResultList <$> o.: "result"
-    parseJSON _ = mzero
+data User = User {
+    user_status         :: Bool,
+    user_result         :: [Pesult]
+    } deriving (Show)
 
+instance FromJSON User where
+  parseJSON = withObject "user" $ \o -> do
+    user_status <- o.: "ok"
+    user_result <- o.: "result"
+    return (User user_status user_result) 
 
-data Result = Result {
-    result_id         :: Int ,
-    result_is_bot     :: Bool ,
-    result_first_name :: String } deriving (Show)
-instance FromJSON Result where
-    parseJSON = withObject "result" $ \o -> do
-      message <- o .: "message"
-      from <- message .: "from"
-      user_id <- from .: "id"
-      user_is_bot <- from .: "is_bot"
-      user_first_name <- from .: "first_name"
-      return (User user_id user_is_bot user_first_name )
+data Pesult = Pesult {
+    id_update :: Int,
+    message   :: Message
+} deriving Show
+
+instance FromJSON Pesult where
+  parseJSON = withObject "result" $ \o -> do
+    id_update <- o.: "update_id"
+    message <- o.: "message"
+    return (Pesult id_update message) 
+
+data Message = Message {
+    id_message  :: Int,
+    from        :: From
+} deriving Show
+
+instance FromJSON Message where
+    parseJSON = withObject "message" $ \o -> do
+      id_message <- o.: "message_id"
+      from <- o.: "from"
+      return (Message id_message from)
+
+data From = From {
+    user_id  :: Int
+} deriving Show
+
+instance FromJSON From where
+    parseJSON = withObject "from" $ \o -> do
+      user_id <- o.: "id"
+      return (From user_id)  
 
 getUser :: Request -> IO (User)
 getUser request = do 
@@ -91,13 +106,13 @@ message_help :: B8.ByteString
 message_help = "Description about this bot"
 
 method_sendHelp :: Int -> B8.ByteString
-method_sendHelp id = makePath (makePath "/sendMessage?chat_id=" (B8.pack.show $ id)) (makePath "&text=" message_help)
+method_sendHelp id = B8.append (B8.append "/sendMessage?chat_id=" (B8.pack.show $ id)) (B8.append "&text=" message_help)
 
 tPath_sendHelp :: Int -> B8.ByteString
-tPath_sendHelp id = makePath token (method_sendHelp id)
+tPath_sendHelp id = B8.append token (method_sendHelp id)
 
 request_sendHelp :: Int -> Request
-request_sendHelp id = buildRequest tHost (tPath_sendHelp id)
+request_sendHelp id = buildRequest host (tPath_sendHelp id)
 
 get_message_response :: Int-> IO (B8.ByteString)
 get_message_response id = do
@@ -106,25 +121,13 @@ get_message_response id = do
 --Problem. I send message from bot to bot!
 --end
 
-
--- make getUpdate request
-method_getUpdate :: B8.ByteString
-method_getUpdate = "/getUpdate"
-
-tPath_getUpdate :: B8.ByteString
-tPath_getUpdate = makePath token method_getUpdate
-
-request_getUpdate :: Request
-request_getUpdate = buildRequest tHost tPath_getUpdate
--- want to update/universied making request methods 
---end
-
-
 main :: IO ()
 main = do
-    user <- getUser request_getMe :: IO (User)
-    iid <- return (is_bot user)
-   -- body <- get_message_response id
-    print (iid )
+  res <- httpBS (request GetUpdate)
+  print (getResponseBody res)
+--  user <- fromJust (decode body :: Maybe User)
+--  print (user)
+     -- body <- get_message_response id
+
 --https://api.telegram.org/bot1421138697:AAHfmKgs38ODbldkqE3jlGEikQlaNuXsOXA/getMe
 
