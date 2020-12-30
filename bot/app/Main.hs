d300db26425150d5ea4c0648eebbe65ec1cfbb08
hovanja2011 <hovanja2012@mail.ru>
2020-12-31 02:46:22 +0300

    rewrite code by myself, can get text of message
    Next step: write sendMessage block,

--- bot/app/Main.hs
+++ bot/app/Main.hs
@@ -1,97 +1,89 @@
 {-# LANGUAGE OverloadedStrings #-}
---{-# LANGUAGE DeriveGeneric #-}
 
 module Main where
 
-import qualified Data.ByteString as B
 import Network.HTTP.Conduit
 import Network.URL
 import Data.Aeson
 import Data.Maybe
 import Control.Monad
-
-baseUrl :: URL
-baseUrl = fromJust $ importURL "https://api.telegram.org"
+import Data.ByteString.Lazy.UTF8 (toString)
 
 type Token = String
 
+baseURL :: URL
+baseURL = fromJust.importURL $ "https://api.telegram.org"
+
 addPath :: String -> URL -> URL
-addPath s url =  url {url_path = oldpath ++ s} where oldpath = url_path url
+addPath path url = url {url_path = oldPath++path} where oldPath = url_path url
 
-withToken :: URL -> Token -> URL
-withToken url t = url {url_path = oldpath ++ "bot" ++ t } where oldpath = url_path url
-
-getMeUrl :: Token -> URL
-getMeUrl t = addPath "/getMe" $ withToken baseUrl t
+addToken :: Token -> URL -> URL
+addToken token url = addPath token $ addPath "bot" baseURL   
 
 makeRequest :: FromJSON a => URL -> IO (Maybe a)
-makeRequest = liftM decode. simpleHttp . exportURL
+makeRequest = liftM decode.simpleHttp.exportURL 
 
-getMe :: Token -> IO (Maybe (TelegramResponse User))
-getMe = makeRequest . getMeUrl
-
-data TelegramResponse a = TelegramResponse
-    { responseOk :: Bool
-    , responseDescription :: Maybe String
-    , responseResult :: Maybe a
-    } deriving Show
-
-instance FromJSON a => FromJSON (TelegramResponse a) where
-    parseJSON (Object v) = TelegramResponse <$>
-                           v .: "ok" <*>
-                           v .:? "description" <*>
-                           v .:? "result"
-    parseJSON _          = mzero 
-
-data User = User
-    { userId :: Int
-    , userFirstName :: String
-    , userLastName :: Maybe String
-    , userUsername :: Maybe String
-    } deriving Show
-
-instance FromJSON User where
-    parseJSON (Object v) = User <$>
-                           v .: "id" <*>
-                           v .: "first_name" <*>
-                           v .:? "last_name" <*>
-                           v .:? "username"
-    parseJSON _          = mzero
-
-data Update = Update
-    { updateId :: Int
-    , updateMessage :: Maybe Message
-    } deriving (Show)
-
-instance FromJSON Update where
-    parseJSON (Object v) = Update <$>
-                           v .: "update_id" <*>
-                           v .:? "message"
-    parseJSON _          = mzero
-
-data Message = Message
-    { messageId :: Int
-    , messageFrom :: User
-    , messageDate :: Int
-    } deriving Show
-
-instance FromJSON Message where
-    parseJSON (Object v) = Message <$>
-                           v .: "message_id" <*>
-                           v .: "from" <*>
-                           v .: "date" 
-    parseJSON _          = mzero    
-
-getUpdatesUrl :: Token -> URL
-getUpdatesUrl t = addPath "/getUpdates" $ withToken baseUrl t
+--Updates block
+getUpdatesURL :: Token -> URL
+getUpdatesURL token = addPath "/getUpdates" $ addToken token baseURL
 
 getUpdates :: Token -> IO (Maybe (TelegramResponse [Update]))
-getUpdates = makeRequest . getUpdatesUrl
+getUpdates token = makeRequest.getUpdatesURL $ token 
+--
+
+data TelegramResponse a = TelegramResponse {
+  responseStatus :: Bool,
+  responseResult :: Maybe a
+} deriving Show
+
+instance FromJSON a => FromJSON (TelegramResponse a) where
+  parseJSON (Object o) = TelegramResponse <$>
+                         o .: "ok" <*>
+                         o .:? "result"
+  parseJSON _          = mzero
+
+data Update = Update {
+  updateId      :: Int,
+  updateMessage :: Maybe Message
+} deriving Show
+
+instance FromJSON Update where
+  parseJSON (Object o) = Update <$>
+                         o.: "update_id" <*>
+                         o.:? "message"
+  parseJSON _          = mzero
+
+data Message = Message {
+  messageId   :: Int,
+  messageFrom :: Maybe User,
+  messageText :: Maybe String
+  } deriving Show
+
+instance FromJSON Message where
+  parseJSON (Object o) = Message <$>
+                         o.: "message_id" <*>
+                         o.:? "from" <*>
+                         o.:? "text"
+  parseJSON _          = mzero
+
+data User = User {
+  userId :: Int,
+  userIsBot :: Bool,
+  userFirstName :: String
+} deriving Show
+
+instance FromJSON User where
+  parseJSON (Object o) = User <$>
+                         o.: "id" <*>
+                         o.: "is_bot" <*>
+                         o.: "first_name"
+  parseJSON _          = mzero
+
+getMessageFromResponse :: TelegramResponse [Update] -> String
+getMessageFromResponse = fromJust.messageText.fromJust.updateMessage.last.fromJust.responseResult 
 
 main :: IO ()
 main = do
-  res <- getUpdates "1421138697:AAHfmKgs38ODbldkqE3jlGEikQlaNuXsOXA" 
-  print (res)
-
---https://api.telegram.org/bot1421138697:AAHfmKgs38ODbldkqE3jlGEikQlaNuXsOXA/getMe
---bot1421138697:AAHfmKgs38ODbldkqE3jlGEikQlaNuXsOXA
+  token <- return ("1421138697:AAHfmKgs38ODbldkqE3jlGEikQlaNuXsOXA")
+  upds <- getUpdates token
+  print (getMessageFromResponse.fromJust $ upds)
